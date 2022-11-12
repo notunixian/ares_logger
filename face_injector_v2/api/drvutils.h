6a2dd63ea72003647a27b8e7bb069040deac32ca
Unixian <48267798+notunixian@users.noreply.github.com>
2022-11-12 13:13:39 -0500

    [injector] add another fallback

--- face_injector_v2/api/drvutils.h
+++ face_injector_v2/api/drvutils.h
@@ -20,7 +20,17 @@ void start_driver()
 		mmap_driver(19);
 
 		driver().handle_driver();
-		driver().is_loaded() ? cout << xor_a("driver retry success!") << endl : cout << xor_a("retry failed, make sure your anti-virus is not blocking the vulnerable driver.") << endl;
+		driver().is_loaded() ? cout << xor_a("driver retry success!") << endl : cout << xor_a("first retry failed") << endl;
+	}
+
+	// fallback incase provider 19 fails.
+	if (!driver().is_loaded())
+	{
+		cout << "driver init retry #2..." << endl;
+		mmap_driver(15);
+
+		driver().handle_driver();
+		driver().is_loaded() ? cout << xor_a("driver retry #2 success!") << endl : cout << xor_a("final retry failed, make sure your anti-virus is not blocking the vulnerable driver.") << endl;
 		if (!driver().is_loaded()) { system("pause"); return; }
 	}
 
