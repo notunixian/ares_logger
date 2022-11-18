dd723bd6aa5941630ba9c855c6bb2f87c4e67ab0
Unixian <48267798+notunixian@users.noreply.github.com>
2022-11-18 14:58:10 -0500

    config stuff

--- ares_logger/main/config/config.cs
+++ ares_logger/main/config/config.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 
 namespace ares_logger.main.config
 {
+    [DataContract]
     public class config
     {
+        // future proofing, i'll add checks to add new values if the version mismatches
+        [DataMember]
+        public int version = 1;
+
+        [DataMember]
         public bool log_avatars {get; set;}
 
+        [DataMember]
         public bool ignore_friends { get; set; }
 
+        [DataMember]
         public bool log_worlds { get; set; }
     }
 }
