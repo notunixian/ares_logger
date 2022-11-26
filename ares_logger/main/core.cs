dacb56db5ba11ff609ef4f402a74e6eb57270f21
notunixian <testezediscord@gmail.com>
2022-11-26 04:46:09 +0000

    finishing touches

--- ares_logger/main/core.cs
+++ ares_logger/main/core.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ares_logger.main
 {
@@ -19,7 +20,7 @@ namespace ares_logger.main
         public static string ares_dir;
         public static void init()
         {
-            Console.Title = "ARES Logger by Unixian";
+            Console.Title = "ARES Logger by Unixian | ver 1.01";
             log_sys.log($"[init]: successfully started ARES Logger", ConsoleColor.Green);
             log_sys.log($"[init]: made with love by unixian", ConsoleColor.Green);
             log_sys.log($"[init]: join the ARES discord server @ discord.gg/vrc-ares", ConsoleColor.Green);
@@ -75,12 +76,20 @@ namespace ares_logger.main
                                 break;
                             }
 
-                            if (args[1].StartsWith("http")) downloader.download(args[1]);
-                            else log_sys.log("[console error]: you did not provide a valid url.");
+                            if (args[1].StartsWith("http") && args[1].Contains("api.vrchat.cloud/api/1/file/")) downloader.download(args[1]);
+                            else log_sys.log("[console error]: you did not provide a valid url.", ConsoleColor.Red);
                         }
                         else 
-                        { 
-                            log_sys.log("[console info]: download ([url to download]) | downloads an avatar based off it's asset url, requires the enable_unsafe_feature flag enabled in your config.", ConsoleColor.Blue); 
+                        {
+                            var text = clipboard.GetText();
+                            log_sys.debug_log(text);
+                            if (text.StartsWith("http") && text.Contains("api.vrchat.cloud"))
+                            {
+                                downloader.download(text);
+                                break;
+                            }
+                            log_sys.debug_log("displaying info because clipboard did not contain valid a valid asset url.");
+                            log_sys.log("[console info]: download ([url]) OR ([input from clipboard]) | downloads an avatar based off it's asset url, requires the enable_unsafe_feature flag enabled in your config.", ConsoleColor.Blue); 
                         }
                         break;
                     }
---
+++ ares_logger/main/util/clipboard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ares_logger.main.util
+{
+    // system forms clipboard never worked
+    // ref: stackoverflow
+    internal class clipboard
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetClipboardData(uint uFormat);
+        [DllImport("user32.dll")]
+        static extern bool IsClipboardFormatAvailable(uint format);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool OpenClipboard(IntPtr hWndNewOwner);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool CloseClipboard();
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GlobalLock(IntPtr hMem);
+        [DllImport("kernel32.dll")]
+        static extern bool GlobalUnlock(IntPtr hMem);
+
+        const uint CF_UNICODETEXT = 13;
+
+        public static string GetText()
+        {
+            if (!IsClipboardFormatAvailable(CF_UNICODETEXT))
+                return null;
+            if (!OpenClipboard(IntPtr.Zero))
+                return null;
+
+            string data = null;
+            var hGlobal = GetClipboardData(CF_UNICODETEXT);
+            if (hGlobal != IntPtr.Zero)
+            {
+                var lpwcstr = GlobalLock(hGlobal);
+                if (lpwcstr != IntPtr.Zero)
+                {
+                    data = Marshal.PtrToStringUni(lpwcstr);
+                    GlobalUnlock(lpwcstr);
+                }
+            }
+            CloseClipboard();
+
+            return data;
+        }
+    }
+}
